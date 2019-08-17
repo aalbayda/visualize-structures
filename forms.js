f9e963f38e2f4076353a22733504379bf1c95073
bob <albraveraphael@gmail.com>
2019-08-18 01:16:39 +0800

    modified form

--- forms.js
+++ forms.js
@@ -2,34 +2,30 @@ const createForm = (structType) => {
     switch(structType) {
         case "Linked list":
             return `
-                <div class="form-group" style="margin-top: 5%">
-                    <label for="select-struct-type">Operation</label>
-                    <div class="row">
-                        <div class="col-sm">
-                            <select class="form-control" id="select-operation" oninput="handleChange()">
-                                <option>Insert at</option>
-                                <option>Delete at</option>
-                            </select>
-                        </div>
-                        <div class="col-sm">
-                            <input class="form-control" type="number" placeholder="Index"></input>
-                        </div>
-                    </div>
-                </div>
+            <div class="form-group" style="margin-top: 5%">
+            <div class="row">
+                <input class="btn btn-dark col-sm" type="button" value="Insert node">
+                <input class="btn btn-dark col-sm" type="button" value="Delete node">
+            </div>
+            </div>
                 `
         case "Stack":
             return `
                 <div class="form-group" style="margin-top: 5%">
-                    <input class="btn btn-dark" type="button" value="Push">
-                    <input class="btn btn-dark" type="button" value="Pop">
+                    <div class="row">
+                        <input class="btn btn-dark col-sm" type="button" value="Push">
+                        <input class="btn btn-dark col-sm" type="button" value="Pop">
+                    </div>
                 </div>
                 `;
         case "Queue":            
             return `
-            <div class="form-group" style="margin-top: 5%">
-            <input class="btn btn-dark" type="button" value="Enqueue">
-            <input class="btn btn-dark" type="button" value="Dequeue">
-        </div>
+                <div class="form-group" style="margin-top: 5%">
+                <div class="row">
+                    <input class="btn btn-dark col-sm" type="button" value="Enqueue">
+                    <input class="btn btn-dark col-sm" type="button" value="Dequeue">
+                </div>
+                </div>
                 `;
         default:
             break;
@@ -41,8 +37,12 @@ const handleChange = () => {
     let structType = e.options[e.selectedIndex].text;
     setTitle(structType);
     //Update card text
-    console.log('handling')
     let form = document.getElementById('form-operations');
     form.innerHTML = createForm(structType);
 }
 
+const handleCopy = () => {
+    let copyText = document.querySelector("textarea");
+    copyText.select();
+    document.execCommand("copy");
+}
